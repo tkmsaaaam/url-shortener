b36d89fb42ba9c29359b27be7b05072ab4df9877
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-06-06 21:05:22 +0900

    add try-catch

--- src/url-shortener.js
+++ src/url-shortener.js
@@ -1,22 +1,27 @@
 javascript:(
   async () =>{
-    var url = '';
-    const locationOrigin = window.location.origin.replace('www.','');
-    var locationPath = window.location.pathname;
-    if (locationOrigin.startsWith('https://amazon')) {
-      var path = locationPath.slice(locationPath.indexOf('/dp/') + 4);
-      path = path.substr(0, path.indexOf('/'));
-      locationPath = '/dp/' + path;
-    };
-    url = locationOrigin + locationPath;
-    await navigator.clipboard.writeText(url).then( () => {
-      alert('copied');
-    }, (err) => {
-      alert(err);
-    });
-    if(window.confirm('transition?')) {
-      window.location.replace(url);
+    try {
+      var url = '';
+      const locationOrigin = window.location.origin.replace('www.','');
+      var locationPath = window.location.pathname;
+      if (locationOrigin.startsWith('https://amazon')) {
+        var path = locationPath.slice(locationPath.indexOf('/dp/') + 4);
+        path = path.substr(0, path.indexOf('/'));
+        locationPath = '/dp/' + path;
+      };
+      url = locationOrigin + locationPath;
+      await navigator.clipboard.writeText(url).then( () => {
+        alert('copied');
+      }, (err) => {
+        alert(err);
+      });
+      if(window.confirm('transition?')) {
+        window.location.replace(url);
+      }
+      return;
+    }catch(e){
+      console.log(e);
+      alert(e);
     }
-    return;
   }
 )();
