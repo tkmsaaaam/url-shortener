e79c2b9e23ffc97512e1941c297a6ebdb655872e
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-06-13 22:03:36 +0900

    npm run format

--- src/url-shortener.js
+++ src/url-shortener.js
@@ -1,26 +1,27 @@
-javascript:(
-  async () =>{
-    try {
-      const locationOrigin = window.location.origin.replace('www.','');
-      var locationPath = window.location.pathname;
-      if (locationOrigin.startsWith('https://amazon')) {
-        var path = locationPath.slice(locationPath.indexOf('/dp/') + 4);
-        path = path.substr(0, path.indexOf('/'));
-        locationPath = '/dp/' + path;
-      };
-      const url = locationOrigin + locationPath;
-      await navigator.clipboard.writeText(url).then( () => {
-        alert('copied');
-      }, (err) => {
-        alert(err);
-      });
-      if(window.confirm('transition?')) {
-        window.location.replace(url);
-      };
-      return;
-    }catch(e){
-      console.log(e);
-      alert(e);
+javascript: (async () => {
+  try {
+    const locationOrigin = window.location.origin.replace("www.", "");
+    var locationPath = window.location.pathname;
+    if (locationOrigin.startsWith("https://amazon")) {
+      var path = locationPath.slice(locationPath.indexOf("/dp/") + 4);
+      path = path.substr(0, path.indexOf("/"));
+      locationPath = "/dp/" + path;
     }
+    const url = locationOrigin + locationPath;
+    await navigator.clipboard.writeText(url).then(
+      () => {
+        alert("copied");
+      },
+      (err) => {
+        alert(err);
+      }
+    );
+    if (window.confirm("transition?")) {
+      window.location.replace(url);
+    }
+    return;
+  } catch (e) {
+    console.log(e);
+    alert(e);
   }
-)();
+})();
