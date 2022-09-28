3f14f43df5f196440fd198cecfc6f14e92d16294
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-09-28 17:50:34 +0900

    update

--- url-shortener/src/script.js
+++ url-shortener/src/script.js
@@ -1,15 +1,18 @@
 (() => {
-  try {
-    const locationOrigin = window.location.origin.replace("www.", "");
-    const locationPath = window.location.pathname;
-    const path = locationPath.slice(locationPath.indexOf("/dp/") + 4);
-    const id = path.substring(0, path.indexOf("/"));
-    if (id) {
-      const url = locationOrigin + "/dp/" + id;
-      window.location.replace(url);
-    }
-  } catch (e) {
-    console.log(e);
-    alert(e);
-  }
+	try {
+		const DP = '/dp/';
+		const originPath = window.location.pathname;
+		if (originPath.indexOf(DP) === -1) return;
+		const dpEndIndex = originPath.indexOf(DP) + DP.length;
+		const path = originPath.slice(dpEndIndex);
+		const idEndIndex = path.indexOf('/');
+		const id = path.substring(0, idEndIndex);
+		if (id) {
+			const url = window.location.origin + DP + id;
+			window.location.replace(url);
+		}
+	} catch (e) {
+		console.log(e);
+		alert(e);
+	}
 })();
